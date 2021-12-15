ec9be591b8b4203ed66b476bf71f6868bfbde00e
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-12-15 18:42:30 +0530

    Create SDBots.py

---
+++ SDBots.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv, fint_dotenv
+
+load_dotenv(find_dotenv())
+
+class SDBots:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+
