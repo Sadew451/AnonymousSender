2ba433cee9701e6aa93e5d6c821bfffe3f5a26b9
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-12-15 18:22:16 +0530

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,13 @@
+import logging
+from pyrogram import Client
+from vars import var
+
+logging.basicconfig(level=logging.INFO)
+
+SD = Client('Anonymous-Sender',
+        api_id=var.API_ID,
+        api_hash=var.API_HASH,
+        bot_token=var.BOT_TOKEN,
+        SDBot=dict(root="SDBot"))
+
+SD.run()
