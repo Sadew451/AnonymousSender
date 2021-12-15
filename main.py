438a391bf123b1756fc81d1aace63c0468740185
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-12-15 17:59:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import logging
+from pyrogram import Client
+from vars import var
+
+logging.basicConfig(level=logging.INFO)
+
+SD = Client('Anonymous-Sender',
+                  api_id=var.API_ID,
+                  api_hash=var.API_HASH,
+                  bot_token=var.BOT_TOKEN,
+                  plugins=dict(root="plugins"))
+
+SD.run()
