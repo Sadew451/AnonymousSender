b153df4a866bd7c5ee031884d119dea2519e2bed
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-12-15 17:29:14 +0530

    Update SDBot.py

--- SDBot.py
+++ SDBot.py
@@ -48,7 +48,8 @@ async def addorno(client, message):
 async def SDBot(client, message):
     await message.copy(message.chat.id)
     disable_web_page_preview=True,
-    
+    quote=True
+
 print("Bot is Started")
 print("Join @SDBotsz.")
 SD.run()
