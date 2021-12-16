84b5afb1ec7d7e3b5bee0aa7dfd3d92899d16186
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-12-16 10:31:35 +0530

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+class var:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
+    API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
+    API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
