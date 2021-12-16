8ff5336324cd9ed4a5c949f1b7c77bed1c52ae2c
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-12-16 10:34:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ SD = Client('Anonymous-Sender',
                   api_id=var.API_ID,
                   api_hash=var.API_HASH,
                   bot_token=var.BOT_TOKEN,
-                  plugins=dict(root="SDBot"))
+                  plugins=dict(root="helper"))
 
 SD.run()
