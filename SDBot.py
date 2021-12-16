b14491cd999c346f4491884e3d99404636b30c98
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-12-16 10:43:02 +0530

    Update SDBot.py

--- SDBot.py
+++ SDBot.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.types.bots_and_keyboards.reply_keyboard_markup import ReplyKeyboardMarkup
 
-API_ID = ("4165223")
-API_HASH = ("972c118766f5e4a76847c1ffdb7d9a04")
-BOT_TOKEN = ("2100965681:AAH_09Shvy-kaHyioHnPJlUa2Y5z88zUbNY")
+API_ID = ("API_ID")
+API_HASH = ("API_HASH")
+BOT_TOKEN = ("BOT_TOKEN")
 
 SD = Client("anymouse sender", bot_token = BOT_TOKEN, api_id = API_ID, api_hash = API_HASH)
 
